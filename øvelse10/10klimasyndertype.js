e4b2a46d42929216aaa2fffc2812a16b9ee0b669
Your Name <youremail@yourdomain.com>
2022-01-31 17:51:18 +0100

    øvelse10øv

---
+++ øvelse10/10klimasyndertype.js
@@ -0,0 +1,18 @@
+let timer = document.querySelector("#timer").value;
+let co2UdledtIKgVedFlyrejse = timer * 109;
+let resultat = `Ved ${timer}timers flyrejse har du udledt ${co2UdledtIKgVedFlyrejse}kg CO2.`;
+
+document.querySelector("button").addEventListener("click", buttonClicked);
+
+function buttonClicked() {
+  if (co2UdledtIKgVedFlyrejse < 100) {
+    resultat += " Tæt på Thunberg";
+  } else if (co2UdledtIKgVedFlyrejse <= 200) {
+    resultat += " Jaja, du behøver ikke at melde dig ud af Å!";
+  } else if (co2UdledtIKgVedFlyrejse <= 600) {
+    resultat += " Det er godt for turismen i nordeuropa";
+  } else {
+    resultat += " Nå ja, vi kan jo altid kolonisere en anden planet...";
+  }
+}
+console.log(resultat);
